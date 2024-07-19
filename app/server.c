01d49ab1d33f07ef999a5b05dc4c4b4180107f2e
maciejspychala <mac.spychala@gmail.com>
2024-07-19 20:13:21 +0200

    dd

--- app/server.c
+++ app/server.c
@@ -7,6 +7,8 @@
 #include <errno.h>
 #include <unistd.h>
 
+#define BUFF_SIZE 1024
+
 int main() {
 	setbuf(stdout, NULL);
 
@@ -51,7 +53,26 @@ int main() {
 	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
 
-	char *resp = "HTTP/1.1 200 OK\r\n\r\n";
+	char buff[BUFF_SIZE] = "";
+	if (recv(client_fd, buff, sizeof(buff), 0) == -1) {
+		printf("Receiving failed: %s\n", strerror(errno));
+		return 1;
+	}
+
+	char verb[BUFF_SIZE] = "";
+	char path[BUFF_SIZE] = "";
+	sscanf(buff, "%s %s", verb, path);
+
+	printf("verb %s\n", verb);
+	printf("path %s\n", path);
+
+	char *ok = "HTTP/1.1 200 OK\r\n\r\n";
+	char *not_found = "HTTP/1.1 404 Not Found\r\n\r\n";
+	char *resp = ok;
+	if (strncmp(path, "/", strlen(path)) != 0) {
+		resp = not_found;
+	}
+
 	if (send(client_fd, resp, strlen(resp), 0) == -1) {
 		printf("Sending failed: %s\n", strerror(errno));
 		return 1;
